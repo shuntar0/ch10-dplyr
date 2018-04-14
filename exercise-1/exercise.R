d270c4da1d290503fb013093a8439a8886708bb8
Shuntaro Tsukazaki <shuntaro@uw.edu>
2018-04-14 00:00:19 -0700

    add answer

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -11,24 +11,24 @@ library(fueleconomy)
 
 # You should now have access to the `vehicles` data frame
 # You can use `View()` to inspect it
-
+View(vehicles)
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
+makes <- vehicles$make
 
 # Use the `unique()` function to determine how many different car manufacturers
 # are represented by the data set
-
+length(unique(makes))
 
 # Filter the data set for vehicles manufactured in 1997
-
+car_1997 <- vehicles[vehicles$year == 1997,]
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
 # Hint: use the `order()` function to get a vector of indices in order by value
 # See also:
 # https://www.r-bloggers.com/r-sorting-a-data-frame-by-the-contents-of-a-column/
-
+car_1997 <- car_1997[order(cars_1997$hwy), ]
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -5,29 +5,30 @@
 # Install and load the "fueleconomy" package
 #install.packages("devtools")
 #devtools::install_github("hadley/fueleconomy")
-library(fueleconomy)
+#library(fueleconomy)
 
 # Install and load the "dplyr" library
-
+#install.packages("dplyr")
+#library(dplyr)
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
+makes <- select(vehicles, make)
 
 # Use the `distinct()` function to determine how many different car manufacturers
 # are represented by the data set
-
+distinct_makes <- distinct(vehicles, make)
 
 # Filter the data set for vehicles manufactured in 1997
-
+cars_1997 <- filter(vehicles, year == 1997)
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
-
+better_hwy_mpg <- arrange(vehicles, -hwy)
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
 
-
+ß
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
